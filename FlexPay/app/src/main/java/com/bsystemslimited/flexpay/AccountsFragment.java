681086e55bf7712e0d302321eb85353210adc55b
jafriyie <j.owusu-afriyie-13@student.lboro.ac.uk>
2015-11-03 16:20:46 +0000

    Swipe View Implemented
    Swipe View with Tabs has been added, but with no design.

---
+++ FlexPay/app/src/main/java/com/bsystemslimited/flexpay/AccountsFragment.java
@@ -0,0 +1,20 @@
+package com.bsystemslimited.flexpay;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Jason on 03/11/2015.
+ */
+public class AccountsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.accounts_fragment, container, false);
+    }
+}
